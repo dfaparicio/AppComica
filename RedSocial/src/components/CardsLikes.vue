142889afc4ad16eb181f67b7fdb6dfc3e3b1ff1f
dfaparicio13 <dfapariciocastro13@gmail.com>
2025-11-23 10:01:44 -0500

    Mejoras en Tendencias1

--- RedSocial/src/components/CardsLikes.vue
+++ RedSocial/src/components/CardsLikes.vue
@@ -3,6 +3,10 @@
 
     <q-img :src="item.fotoPerfil" class="card-img">
 
+      <img :src="Like" class="like-icon like-top-left" />
+      <img :src="Like" class="like-icon like-top-right" />
+      <img :src="Like" class="like-icon like-bottom-left" />
+      <img :src="Like" class="like-icon like-bottom-right" />
 
       <div class="absolute-bottom text-subtitle2 text-center">
 
@@ -27,7 +31,13 @@
   </q-card>
 </template>
 
+
+
 <script setup>
+
+import { QImg, QCard } from "quasar";
+import Like from "@/assets/Likes.png";
+
 defineProps({
   item: {
     type: Object,
@@ -59,5 +69,37 @@ defineProps({
 
 .card-img {
   height: 250px;
+}
+
+.like-icon {
+  position: absolute;
+  width: 26px;
+  height: 26px;
+  z-index: 20;
+  object-fit: contain;
+}
+
+.like-top-left {
+  top: 2px;
+  left: 2px;
+  transform: rotate(-50deg);
+}
+
+.like-top-right {
+  top: 2px;
+  right: 2px;
+   transform: rotate(50deg);
+}
+
+.like-bottom-left {
+  bottom: 2px;
+  left: 2px;
+   transform: rotate(50deg);
+}
+
+.like-bottom-right {
+  bottom: 2px;
+  right: 2px;
+  transform: rotate(-50deg);
 }
 </style>
